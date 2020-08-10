ecb08447d350b4b84ebe9dfc12ef7ffb1ae7d79e
hays30@naver.com <@gktjdgur159>
2020-08-10 20:09:50 +0900

    pulse sensor

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,5 +1,12 @@
+
 #include <SoftwareSerial.h>
-SoftwareSerial BTSerial(4, 5);  // (Tx, Rx)
+
+// HM-10 pins
+#define BLE_RX (4)
+#define BLE_TX (5)
+
+// HM-10 module instance
+SoftwareSerial bleSerial(BLE_RX, BLE_TX);
 
 // joystick sensor values
 #define VR_MIN_VALUE (0)
@@ -15,42 +22,66 @@ SoftwareSerial BTSerial(4, 5);  // (Tx, Rx)
 #define IN3 9
 #define IN4 10
 
+// The minimum value of PWM to drive an DC motor.
+// The value should be set according to the motor characteristics
+// of a vehicle. In my system, left motor works above 95 whereas
+// right motor moves above 85. To drive the car as straight as
+// possible, I make motors stop under 95.
+//
 #define MOTOR_MIN_PWM (95)
 #define MOTOR_MAX_PWM (255)
 
+/*
+   Convert a value from joystick to appropriate PWM value.
+
+   @param value a integer value between 0 and 1023 read from joystick.
+   @returns a mapped integer value between -255 and 255 which used to
+            drive a motor.
+*/
 int convertToPWM(int value) {
   return map(value, VR_MIN_VALUE, VR_MAX_VALUE, -MOTOR_MAX_PWM, MOTOR_MAX_PWM);
 }
 
+/*
+   Drives two motors according to the given pwm values.
 
+   To avoid write several functions for each motor direction, I simply
+   drive motor forward if the given value is positive and backward if
+   negative. To stop a motor, just give 0.
+
+   Since every DC motors have differenct characteristics, I stop a
+   motor if the absolute value of the given pwm value is less than
+   MOTOR_MIN_PWM to drive the vehicle as strait as possible.
+
+   @param pwmL PWM value of left motor. An integer value between
+               -255 and 255.
+   @param pwmR PWM value of right motor. An integer value between
+               -255 and 255.
+*/
 void drive(int pwmL, int pwmR) {
   // constrain motor speed between -255 and 255
   pwmL = constrain(pwmL, -MOTOR_MAX_PWM, MOTOR_MAX_PWM);
   pwmR = constrain(pwmR, -MOTOR_MAX_PWM, MOTOR_MAX_PWM);
-  
+
   // left motor direction
   if (pwmL > 0) {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
-    analogWrite(IN1, 255);
   } else if (pwmL < 0) {
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
-    analogWrite(IN2, 255);
   } else {
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
   }
-  
+
   // right motor direction
   if (pwmR > 0) {
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
-    analogWrite(IN3, 255);
   } else if (pwmR < 0) {
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, HIGH);
-    analogWrite(IN4, 255);
   } else {
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
@@ -59,15 +90,16 @@ void drive(int pwmL, int pwmR) {
   // stop motors if the absolute value of pwm is less than MOTOR_MIN_PWM
   if (abs(pwmL) <= MOTOR_MIN_PWM) pwmL = 0;
   if (abs(pwmR) <= MOTOR_MIN_PWM) pwmR = 0;
-  
+
   // pwm for each motor.
   analogWrite(ENA, abs(pwmL));
   analogWrite(ENB, abs(pwmR));
 }
 
 void setup() {
+  // set up HM-10 module
   Serial.begin(9600);
-  BTSerial.begin(9600);
+  bleSerial.begin(9600);
 
   // set all the motor pins OUTPUT mode
   pinMode(ENA, OUTPUT);
@@ -76,62 +108,36 @@ void setup() {
   pinMode(ENB, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-
-  //pinMode(6, OUTPUT);
-  //pinMode(7, OUTPUT);
-  //pinMode(8, OUTPUT);
 }
 
 void loop() {
-  //String str =  BTSerial.read();
+  // received controll command
+  if (bleSerial.available() > 0) {
+    // read joystick values from analog pins
+    int vrx = bleSerial.parseInt();
+    int vry = bleSerial.parseInt();
 
-  //Serial.println(var);
-  int flag = BTSerial.parseInt();
-  int vrx = BTSerial.parseInt();
-  int vry = BTSerial.parseInt();
-  int vrz = BTSerial.parseInt();
 
-  int pwmX = convertToPWM(vrx);
-  int pwmL = pwmX;
-  int pwmR = pwmX;
-  
-  // adjust left and right speed according to the y axis
-  int pwmY = convertToPWM(vry);
-  pwmL = pwmL + pwmY;
-  pwmR = pwmR - pwmY;
+    // calculate speed of both motors according to the x axis
+    int pwmX = convertToPWM(vrx);
+    int pwmL = pwmX;
+    int pwmR = pwmX;
+
+    // adjust left and right speed according to the y axis
+    int pwmY = convertToPWM(vry);
+    pwmL = pwmL + pwmY;
+    pwmR = pwmR - pwmY;
+
+    Serial.print(vrx);
+    Serial.print(',');
+    Serial.print(vry);
+    Serial.print(" : ");
+    Serial.print(pwmL);
+    Serial.print(',');
+    Serial.println(pwmR);
     
-  // happy drive!
-  drive(pwmL, pwmR);
 
-  /*
-  if (vrx < 200){
-    digitalWrite(6, HIGH);
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
+    // happy drive!
+    drive(pwmL, pwmR);
   }
-  if (vrx >= 200 && vrx < 700){
-    digitalWrite(7, HIGH);
-    digitalWrite(6, LOW);
-    digitalWrite(8, LOW);
-  }
-  if (vrx > 700){
-    digitalWrite(8, HIGH);
-    digitalWrite(7, LOW);
-    digitalWrite(6, LOW);
-  }
-*/
-  Serial.print(flag);
-  Serial.print(',');
-  Serial.print(vrx);
-  Serial.print(',');
-  Serial.print(vry);
-  Serial.print(',');
-  Serial.println(vrz);
-  
-
-  /*
-    if (BTSerial.available())
-    Serial.write(BTSerial.read());
-    //if (Serial.available())
-    //BTSerial.write(Serial.read());*/
 }
--- sender/sender.ino
+++ sender/sender.ino
@@ -1,5 +1,15 @@
 #include <SoftwareSerial.h>
+#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
 
+//  Variables
+const int PulseWire = 3;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+// Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+// Otherwise leave the default "550" value.
+
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
 
 // HM-10 pins
 #define BLE_RX (4)
@@ -11,7 +21,7 @@
 #define VRZ (A2)
 
 // pluse pins
-#define PulsePin (A5)
+#define PulsePin (A3)
 
 // joystick sensor values
 #define VR_MIN_VALUE (0)
@@ -21,6 +31,7 @@
 #define VR_MID_LOWER (460)
 
 SoftwareSerial BTSerial(BLE_RX, BLE_TX);
+const int delayMsec = 60; // 100msec per sample
 
 int adjustJoystickValue(int value) {
   value = constrain(value, VR_MIN_VALUE, VR_MAX_VALUE);
@@ -36,31 +47,61 @@ void setup() {
   pinMode(VRZ, INPUT);
   Serial.begin(9600);
   BTSerial.begin(9600);
+
+  // Configure the PulseSensor object, by assigning our variables to it.
+  pulseSensor.analogInput(PulseWire);
+  pulseSensor.blinkOnPulse(LED13);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);
+
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal.
+  if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.
+  }
+
 }
 
 void loop() {
 
   int vrx = analogRead(VRX); //analogRead(A0)
   int vry = analogRead(VRY); //analogRead(A1)
-  int vrz = analogRead(VRZ); //analogRead(A2)
+  //int vrz = analogRead(VRZ); //analogRead(A2)
+  //int value = analogRead(PulsePin);
 
   vrx = adjustJoystickValue(vrx);
   vry = adjustJoystickValue(vry);
-  vrz = adjustJoystickValue(vrz);
+  //vrz = adjustJoystickValue(vrz);
+
+  //BTSerial.print(1);
+  //BTSerial.print(",");
+  BTSerial.print(vrx);
+  BTSerial.print(",");
+  BTSerial.println(vry);
+  //BTSerial.print(",");
+  //BTSerial.println(vrz);
+  delay(100);
+
+  int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
+  // "myBPM" hold this BPM value now.
+
+  if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
+   // Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
+    //Serial.print("BPM: ");                        // Print phrase "BPM: "
+   // Serial.println(myBPM);                        // Print the value inside of myBPM.
+  }
+
+  //delay(1000);                    // considered best practice in a simple sketch.
+
+
 
   Serial.print(vrx);
   Serial.print(',');
-  Serial.print(vry);
-  Serial.print(',');
-  Serial.println(vrz);
-  delay(100);
+  Serial.println(vry);
+  //Serial.print(',');
+  //Serial.println(vrz);
 
-  BTSerial.print(1);
-  BTSerial.print(",");
-  BTSerial.print(vrx);
-  BTSerial.print(",");
-  BTSerial.print(vry);
-  BTSerial.print(",");
-  BTSerial.println(vrz);
+  //Serial.println(value);
+
+
+  
 
 }
