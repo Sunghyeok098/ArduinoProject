78fdb5d365082830d7464a2879fbfabdfe355ccc
Sunghyeok098 <hays39@naver.com>
2020-08-14 15:37:30 +0900

    edit sensor

--- sender/sender.ino
+++ sender/sender.ino
@@ -3,7 +3,7 @@
 #include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
 
 //  Variables
-const int PulseWire = 3;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int PulseWire = A0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
 const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
 int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
 // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
@@ -16,9 +16,9 @@ PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPla
 #define BLE_TX (5)
 
 // joystick pins
-#define VRX (A0)
-#define VRY (A1)
-#define VRZ (A2)
+#define VRX (A1)
+#define VRY (A2)
+#define VRZ (A3)
 
 // pluse pins
 #define PulsePin (A3)
@@ -78,24 +78,24 @@ void loop() {
   BTSerial.println(vry);
   //BTSerial.print(",");
   //BTSerial.println(vrz);
-  delay(100);
+  delay(20);
 
   int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
   // "myBPM" hold this BPM value now.
 
   if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened".
-   // Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
-    //Serial.print("BPM: ");                        // Print phrase "BPM: "
-   // Serial.println(myBPM);                        // Print the value inside of myBPM.
+    Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
+    Serial.print("BPM: ");                        // Print phrase "BPM: "
+    Serial.println(myBPM);                        // Print the value inside of myBPM.
   }
 
   //delay(1000);                    // considered best practice in a simple sketch.
 
 
 
-  Serial.print(vrx);
-  Serial.print(',');
-  Serial.println(vry);
+  //Serial.print(vrx);
+  //Serial.print(',');
+  //Serial.println(vry);
   //Serial.print(',');
   //Serial.println(vrz);
 
